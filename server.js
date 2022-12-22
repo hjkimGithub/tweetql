6ec826f60657bdf2a4b034310a2874746d2da639
김현재 <rlaguswo3408@gmail.com>
2022-12-22 20:28:04 +0900

    오늘의 삽질, apollo-server, apollo-server-express 는 listen 방법이 다르다...

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServer, gql } from "apollo-server";
+
 // package.json 상 "type": "module" 있으면 위와같이 가능
 // const {ApolloServer, gql} = require("apollo-server")
 
 // requirement: typeDefs
 // SDL: gql `SDL` : Schema Definition Language
 // Query type: Mandatory
-const typeDefs = gql `  
-    type Query {
-        text: String
-    }
+
+const typeDefs = gql`
+  type Query {
+    text: String
+    hello: String
+  }
 `;
 
-const server = new ApolloServer({typeDefs});
+const server = new ApolloServer({ typeDefs });
 
-server.listen().then(({url}) => {
-    console.log(`Running on ${url}`);
-})
-
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
+});
