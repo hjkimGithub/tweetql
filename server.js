d17bc5546405436aba265d5afb7902aa41df2722
김현재 <rlaguswo3408@gmail.com>
2022-12-26 15:49:54 +0900

    Query, simple mutation

--- server.js
+++ server.js
@@ -7,14 +7,64 @@ import { ApolloServer, gql } from "apollo-server";
 // SDL: gql `SDL` : Schema Definition Language
 // Query type: Mandatory
 
+const tweets = [
+    {
+        id: "1",
+        text: "first",
+    },
+    {
+        id: "2",
+        text: "second",
+    },
+];
+
 const typeDefs = gql`
-  type Query {
-    text: String
-    hello: String
-  }
+
+    type User {
+        id: ID!
+        username: String!
+        firstName: String!
+        lastName: String
+    }
+    type Tweet {
+      id: ID!
+      text: String!
+      author: User
+    }
+
+    type Query {
+      allTweets: [Tweet!]!
+      tweet(id: ID!): Tweet
+    }
+    
+    type Mutation {
+        postTweet(text: String!, userId: ID!): Tweet!
+        deleteTweet(id: ID!): Boolean!
+    }
 `;
 
-const server = new ApolloServer({ typeDefs });
+/*
+    GET /api/v1/tweet
+    GET /Api/v1/tweet/:id
+
+    POST DELETE PUT /api/v1/tweets ~~
+*/
+
+// const server = new ApolloServer({ typeDefs });
+
+const resolvers = {
+    Query: {
+        allTweets() {
+            return tweets;
+        },
+        tweet(root, {id}) {
+            console.log(id);
+            return tweets.find((tweet) => tweet.id === id);
+        },
+    },
+};
+
+const server = new ApolloServer({typeDefs, resolvers})
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
